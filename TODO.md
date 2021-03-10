fe4a2beb5660c881ffa93a1a11102dee8e8159e8
Kyle Goetke <kylegoetke@protonmail.com>
2021-03-10 08:23:26 -0500

    Refactored the entire project
    - Fixed visual errors
        - Total horizontal width is now equal
        - Input is now spaced correctly within the box
        - Borders between elements are now equal
    - Simplified the calculation method
    - Condensed and cleaned functions
    - Made the input an editable text box

---
+++ TODO.md
@@ -0,0 +1,14 @@
+### validity checks (done?)
+- need to make sure the previous character(s) are valid when cicking an operation key
+    - e.g., you can't have just "/12", there needs to be smth like "36/12"
+- only one decimal: use .contains(".")?
+
+### visuals
+- input inside div
+    - center?
+    - fill parent?
+- looping text
+    - horizontal scrollbar
+    - stays on right side when adding text (might be like this by default, idrk)
+- equation display
+    - add or input or whatever to left side of screen
--- index.js
+++ index.js
@@ -1,247 +1,222 @@
-var input = 0;
-var display = 0;
-var yarn = "";
-var dot = 0;
-var operator = 0;
-var checkChange = 1;
-var checkZero = 0;
+input = 0;
+displayed = "";
+endMessage = false;
 
 window.onload = function start() {
-	document.getElementById("display").innerHTML = display;
+	document.getElementById("output").value = displayed;
 };
 
-function cleardisplay() {
-	yarn = "";
-  display = 0;
-	dot = 0;
-	checkZero = 0;
-	document.getElementById("display").innerHTML = display;
+function getDisplay() {
+	displayed = document.getElementById("output").value.toString();
 }
 
-function changesign() {
-	if (checkChange===1) {
-		input = display;
-		yarn = input * -1;
-		yarn = yarn.toString();
-	  document.getElementById("display").innerHTML = yarn;
-	  display = yarn;
-	}else if(checkChange===0){
-		input = "-";
-		yarn = display;
-		yarn = yarn + input;
-		yarn = yarn.toString();
-		document.getElementById("display").innerHTML = yarn;
-		display = yarn;
+function updateDisplay() {
+	document.getElementById("output").value = displayed;
+	console.log(displayed);
+}
+
+function clearDisplay() {
+	displayed = "";
+	input = 0;
+	endMessage = true;
+	document.getElementById("output").value = displayed;
+}
+
+function enterKey(event) {
+	if (event.keyCode == 13) {
+		getDisplay();
+		equals();
 	}
 }
 
-function percent() {
-	operator = 0;
-  input = display;
-	yarn = input / 100;
-	yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
-}
-
-function zero() {
-  if(checkZero===1) {
-		input = 0;
-	  operator = 0;
-		checkChange = 1;
-	  yarn = yarn + input;
-	  yarn = yarn.toString();
-	  document.getElementById("display").innerHTML = yarn;
-	  display = yarn;
-	}else{
+function nine() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
 	}
-}
-
-function one() {
-  input = 1;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
-}
-
-function two() {
-  input = 2;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
-}
-
-function three() {
-  input = 3;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
-}
-
-function four() {
-  input = 4;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
-}
-
-function five() {
-  input = 5;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
-}
-
-function six() {
-  input = 6;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
-}
-
-function seven() {
-  input = 7;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
+	input = "9";
+	displayed += input;
+	updateDisplay();
 }
 
 function eight() {
-  input = 8;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "8";
+	displayed += input;
+	updateDisplay();
 }
 
-function nein() {
-  input = 9;
-  operator = 0;
-	checkZero = 1;
-	checkChange = 1;
-  yarn = yarn + input;
-  yarn = yarn.toString();
-  document.getElementById("display").innerHTML = yarn;
-  display = yarn;
+function seven() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "7";
+	displayed += input;
+	updateDisplay();
+}
+
+function six() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "6";
+	displayed += input;
+	updateDisplay();
+}
+
+function five() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "5";
+	displayed += input;
+	updateDisplay();
+}
+
+function four() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "4";
+	displayed += input;
+	updateDisplay();
+}
+
+function three() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "3";
+	displayed += input;
+	updateDisplay();
+}
+
+function two() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "2";
+	displayed += input;
+	updateDisplay();
+}
+
+function one() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "1";
+	displayed += input;
+	updateDisplay();
+}
+
+function zero() {
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "";
+		endMessage = false;
+	}
+	input = "0";
+	displayed += input;
+	updateDisplay();
 }
 
 function decimal() {
-  if (dot===0) {
-  	input = ".";
-		yarn = display;
-		yarn = yarn + input;
-		yarn = yarn.toString();
-		document.getElementById("display").innerHTML = yarn;
-		display = yarn;
-		dot = 1;
-		checkZero = 1;
-	}else{
+	getDisplay();
+	if (displayed === "0" || endMessage) {
+		displayed = "0";
+		endMessage = false;
 	}
+	input = ".";
+	displayed += input;
+	updateDisplay();
 }
 
-function plus() {
-  if (operator===0) {
-  	dot = 0;
-		input = "+";
-		yarn = display;
-  	yarn = yarn + input;
-  	yarn = yarn.toString();
-  	document.getElementById("display").innerHTML = yarn;
-  	display = yarn;
-		operator = 1;
-		checkChange = 0;
-	}else{
-	}
-}
-
-function subtract() {
-  if (operator===0) {
-  	dot = 0;
-		input = "-";
-		yarn = display;
-  	yarn = yarn + input;
-  	yarn = yarn.toString();
-  	document.getElementById("display").innerHTML = yarn;
-  	display = yarn;
-		operator = 1;
-		checkChange = 0;
-	}else{
+function divide() {
+	getDisplay();
+	lastInt = parseInt(displayed.slice(-1))
+	if (!isNaN(lastInt)) {
+		input = "/";
+		displayed += input;
+		updateDisplay();
 	}
 }
 
 function multiply() {
-  if (operator===0) {
-  	dot = 0;
+	getDisplay();
+	lastInt = parseInt(displayed.slice(-1))
+	if (!isNaN(lastInt)) {
 		input = "*";
-		yarn = display;
-  	yarn = yarn + input;
-  	yarn = yarn.toString();
-  	document.getElementById("display").innerHTML = yarn;
-  	display = yarn;
-		operator = 1;
-		checkChange = 0;
-	}else{
+		displayed += input;
+		updateDisplay();
 	}
 }
 
-function divide() {
-  if (operator===0) {
-  	dot = 0;
-		input = "/";
-		yarn = display;
-  	yarn = yarn + input;
-  	yarn = yarn.toString();
-  	document.getElementById("display").innerHTML = yarn;
-  	display = yarn;
-		operator = 1;
-		checkChange = 0;
-	}else{
+function subtract() {
+	getDisplay();
+	lastInt = parseInt(displayed.slice(-1))
+	if (!isNaN(lastInt)) {
+		input = "-";
+		displayed += input;
+		updateDisplay();
 	}
+}
+
+function add() {
+	getDisplay();
+	lastInt = parseInt(displayed.slice(-1))
+	if (!isNaN(lastInt)) {
+		input = "+";
+		displayed += input;
+		updateDisplay();
+	}
+}
+
+function changeSign() {
+	getDisplay();
+	first = displayed.substring(0, 1);
+	if (first !== "-") {
+		displayed = "-" + displayed;
+	} else {
+		displayed = displayed.substring(1);
+	}
+	updateDisplay();
+}
+
+function percent() {
+	getDisplay();
+	if (endMessage) {
+		endMessage = false;
+	}
+	displayed += "/100";
+	displayed = eval(displayed);
+	updateDisplay();
 }
 
 function equals() {
-	var total = 0;
-	dot = 0;
-  total = yarn.toString();
-  yarn = "";
-  total = eval(total);
-	if (total===Infinity || total===NaN || total===undefined) {
-		document.getElementById("display").innerHTML = "Error encountered!";
-	}else{
-		total = parseFloat(total.toFixed(10));
-		total = total.toString();
-	  document.getElementById("display").innerHTML = total;
-	  display = total;
+	getDisplay();
+	displayed = eval(displayed)
+	if (displayed % 1 != 0) {
+		displayed = displayed.toFixed(2);
 	}
+	endMessage = true;
+	updateDisplay();
 }
